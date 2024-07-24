a726676c76efe0d7975c50127851baa0234a7aee
Og'abek542 <123234816+ogabek542@users.noreply.github.com>
2024-07-24 18:51:00 +0500

    add search button

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+// import Pages 
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
+
+export default App;
